ce2f944d9c3d2420ce9ab26319033da995124162
muhamadayeshaaulia <shaxyyy.03@gmail.com>
2025-04-23 13:23:24 +0700

    update

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -86,5 +86,31 @@ export const logout =  (req, res) =>{
 };
 
 export const updateProfile = async (req , res) => {
+    try {
+        const {profilePic}= req.body;
+        const userId = req.user._id;
 
+        if(!profilePic){
+            return res.status(400).json({message: "Profile pic is not required"});
+        }
+        const uploadResponse = await cloudinary.uploader.upload(profilePic);
+        const updatedUser = await User.findByIdAndUpdate(
+            userId, 
+            {profilePic:uploadResponse.secure_url},
+            {new:true});
+
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        console.log("error in update profile: ", error);
+        res.status(500).json({message: "Internal server error"});
+    }
+};
+
+export const checkAuth = (req, res) => {
+    try {
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.log("error in checkAuth controller", error.message);
+        res.status(500).json({message: "Internal Server Error"});
+    }
 };
--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import {connectDB} from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+
+
 dotenv.config();
 const app = express();
 
