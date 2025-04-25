951b8d57a40ccb7c3a8f97fcaccacbf9c3580f1a
muhamadayeshaaulia <shaxyyy.03@gmail.com>
2025-04-25 23:14:56 +0700

    Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,7 +24,6 @@ const App = () => {
     <Routes>
       <Route path="/" element= { <HomePage /> } />
       <Route path="/signup" element= { <SignUpPage /> } />
-      <Route path="/signup" element= { <SignUpPage /> } />
       <Route path="/login" element= { <LoginPage /> } />
       <Route path="/settings" element= { <SettingsPage /> } />
       <Route path="/profile" element= { <ProfilePage /> } />
