71b0252fa040f47b494c68614887eb8231359483
muhamadayeshaaulia <shaxyyy.03@gmail.com>
2025-04-25 23:13:08 +0700

    anyar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,6 +24,7 @@ const App = () => {
     <Routes>
       <Route path="/" element= { <HomePage /> } />
       <Route path="/signup" element= { <SignUpPage /> } />
+      <Route path="/signup" element= { <SignUpPage /> } />
       <Route path="/login" element= { <LoginPage /> } />
       <Route path="/settings" element= { <SettingsPage /> } />
       <Route path="/profile" element= { <ProfilePage /> } />
