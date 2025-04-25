1cca36a1374c48484ed5d575305a2557a224b875
muhamadayeshaaulia <shaxyyy.03@gmail.com>
2025-04-25 23:07:06 +0700

    Update ilagin test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,13 +22,11 @@ const App = () => {
     <Navbar/>
 
     <Routes>
-    <Route path="/" element= { <HomePage /> } />
-      <Route path="test" element= { <TestPage /> } />
+      <Route path="/" element= { <HomePage /> } />
       <Route path="/signup" element= { <SignUpPage /> } />
       <Route path="/login" element= { <LoginPage /> } />
       <Route path="/settings" element= { <SettingsPage /> } />
       <Route path="/profile" element= { <ProfilePage /> } />
-
     </Routes>
 
   </div>
